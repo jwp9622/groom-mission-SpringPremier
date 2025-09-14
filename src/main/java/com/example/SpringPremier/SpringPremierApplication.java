045fc758e159049e41d1dc6bbc25b69b38fafdb8
=박다희 <jwp9622@naver.com>
2025-09-14 22:06:49 +0900

    [#1] refactor: cicd .env file edit (#37)

--- src/main/java/com/example/SpringPremier/SpringPremierApplication.java
+++ src/main/java/com/example/SpringPremier/SpringPremierApplication.java
@@ -12,7 +12,11 @@ public class SpringPremierApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure().load();
+		// 수정 코드 (안전하게 사용)
+		Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMissing()  // ✅ .env 없으면 무시
+			.load();
+
 		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		System.setProperty("SPRING_SECURITY_USER_NAME", dotenv.get("SPRING_SECURITY_USER_NAME"));
